3ab1200f2f90eb085e8ac671e8a19b7bc1321b6d
Steve Mitchell <smitchell@bandwidth.com>
2015-07-09 16:40:50 -0600

    make from number TTS friendly

--- web/outbound_call_handler.php
+++ web/outbound_call_handler.php
@@ -29,6 +29,6 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
     <Gather maxDigits="1" requestUrl="/outbound_gather_handler.php?conferenceNumber=<?php echo urlencode($conferenceNumber) ?>&amp;inboundCallId=<?php echo $inboundCallId ?>"  >
-      <SpeakSentence gender="female" locale="en_US" voice="julie">You have a call from <?php echo $from ?>. Press one to answer the call or two to send to voice mail.</SpeakSentence>
+      <SpeakSentence gender="female" locale="en_US" voice="julie">You have a call from <?php echo $ttsFriendly ?>. Press one to answer the call or two to send to voice mail.</SpeakSentence>
    </Gather>
 </Response>
